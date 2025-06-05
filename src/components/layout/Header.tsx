5deaf1d9440295c6f01580ff935051f2dde8f8ce
Leonardo Suasnavar <leosuasnavar@gmail.com>
2025-06-05 05:24:54 +0000

    bueno volvamos a lo de antes, vuelve a agregar el admin

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ const navItems = [
   { href: '/participants', label: 'EQUIPOS' },
   { href: '/competition', label: 'PARTIDOS' },
   { href: '/contact', label: 'CONTACTO' },
-  // { href: '/admin', label: 'ADMIN', icon: Cog }, // Removed Admin link
+  { href: '/admin', label: 'ADMIN', icon: Cog }, 
 ];
 
 export function Header() {
